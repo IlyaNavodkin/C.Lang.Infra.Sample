0874c05f8569224c35366779d2134602f15949e9
ilyas <ilyas@CHAMION>
2025-05-04 15:30:20 +0300

    Fix tasks

--- src/main.c
+++ src/main.c
@@ -2,16 +2,23 @@
 #include <string.h>
 #include <windows.h>
 
-int main() {
+void SetupConsoleSettings()
+{
     SetConsoleOutputCP(65001); 
     SetConsoleCP(65001);      
+}
+
+int main() 
+{
+    SetupConsoleSettings();
 
     char name[100];
-    printf("Здарова епта, имя назови свое?\n");
+    printf("АЛОО Здарова епта, имя назови свое?\n");
     fgets(name, sizeof(name), stdin);
     name[strcspn(name, "\n")] = 0;
 
     printf("Здарова епта, %s!\n", name);
+    printf("дАВАЙ Бывай");
 
     return 0;
 }
